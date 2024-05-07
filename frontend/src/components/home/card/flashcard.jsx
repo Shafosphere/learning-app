f1b9cc3cba4a9ea17fb5701700d9ab6f80086134
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-05-07 20:23:26 +0200

    Version 0.2 - working flashcard

--- frontend/src/components/home/card/flashcard.jsx
+++ frontend/src/components/home/card/flashcard.jsx
@@ -1,14 +1,55 @@
+import { useEffect, useState } from "react";
 import "./flashcard.css";
-export default function Flashcard({data}) {
+export default function Flashcard({ data, check, className }) {
+  const [plWord, setWord] = useState(data.wordPl.word);
+  const [userWord, setUserWord] = useState("");
+  const wordLength = plWord.length;
+  function handleInputChange(event) {
+    setUserWord(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault(); // Zapobiega domyślnemu zachowaniu formularza
+    check(userWord, plWord);
+  }
+  useEffect(() => {
+    setWord(data.wordPl.word);
+    setUserWord("");
+  }, [data.wordPl.word]);
 
   return (
     <>
       <div className="container-flashcard">
-        <div className="window-flashcard">
-          <span>word</span>
-          <input></input>
-        </div>
-        <div className="flashcard-description"></div>
+        <form onSubmit={handleSubmit}>
+          <div className="top-flashcard">
+            <div className={`window-flashcard ${className}`}>
+              <span>{data.wordEng.word}</span>
+              <input
+                type="text"
+                style={{ "--wordLength": wordLength }}
+                maxLength={wordLength}
+                value={userWord}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="button-container">
+              <button
+                onClick={() => check(userWord, plWord)}
+                className="button"
+                type="submit"
+                style={{ "--buttonColor": "var(--highlight)" }}
+              >
+                submit
+              </button>
+              <button
+                className="button"
+                style={{ "--buttonColor": "var(--secondary)" }}
+              >
+                hint
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="flashcard-description">{data.wordPl.description}</div>
       </div>
     </>
   );
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,16 +1,45 @@
-import "./home.css"
-import Flashcard from "../../components/home/card/flashcard"
-import { data } from "../../data/data"
-import { useState } from "react";
-export default function Home(){
-    let wordNumber = 20;
-    const[wordsData, setWord ] = useState(data.slice(0, wordNumber))
-    const[randomWord, setRandom] = useState('');
-    return (
-        <div className="container-home">
-            <div className="window-home">
-                <Flashcard data={wordsData}/>
-            </div>
-        </div>
-    )
+import "./home.css";
+import Flashcard from "../../components/home/card/flashcard";
+import { data } from "../../data/data";
+import { useEffect, useState } from "react";
+export default function Home() {
+  let wordNumber = 20;
+  const [wordsData, setWord] = useState(data.slice(0, wordNumber));
+  const [randomWord, setRandom] = useState("");
+  const [className, setClass] = useState("");
+
+  function selectRandomWord() {
+    const randomIndex = Math.floor(Math.random() * wordsData.length);
+    setRandom(wordsData[randomIndex]);
+  }
+
+  function check(userWord, word) {
+    if (userWord === word) {
+      setClass("correct");
+      setTimeout(() => {
+        setClass("");
+        selectRandomWord();
+      }, 3000);
+    } else {
+      setClass("notcorrect");
+      setTimeout(() => {
+        setClass("");
+        selectRandomWord();
+      }, 3000);
+    }
+  }
+
+  useEffect(() => {
+    selectRandomWord();
+  }, []);
+
+  return (
+    <div className="container-home">
+      <div className="window-home">
+        {randomWord && (
+          <Flashcard data={randomWord} check={check} className={className} />
+        )}
+      </div>
+    </div>
+  );
 }
