917ec13818effddb791be8ca8e5b2ba146041aa8
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-06-10 17:10:56 +0200

    settings css

--- frontend/src/pages/settings/settings.jsx
+++ frontend/src/pages/settings/settings.jsx
@@ -4,13 +4,17 @@ import { SettingsContext } from "./properties";
 import Popup from "../../components/popup/popup";
 
 export default function Settings() {
-  const { dailyGoal, setDailyGoal, resetDateIfNeeded } = useContext(SettingsContext);
+  const { dailyGoal, setDailyGoal, resetDateIfNeeded } =
+    useContext(SettingsContext);
 
   const [newDailyGoal, setNewDailyGoal] = useState(dailyGoal);
 
-  //popup
+  // popup
   const [popupMessage, setPopupMessage] = useState("");
   const [popupEmotion, setPopupEmotion] = useState("");
+
+  // span management
+  const [activeSpan, setSpan] = useState("");
 
   function handleDailyGoalChange(e) {
     setNewDailyGoal(e.target.value);
@@ -19,48 +23,82 @@ export default function Settings() {
   function saveSettings() {
     setDailyGoal(newDailyGoal);
     setPopupEmotion("positive");
-    setPopupMessage("settings saved");
+    setPopupMessage("Settings saved");
   }
 
   useEffect(() => {
     resetDateIfNeeded();
   }, [resetDateIfNeeded]);
 
-
   return (
     <div className="container-settings">
       <div className="window-settings">
-        <div className="switches">
-          <div className="switch-container">
-            <span className="switch-text">Sounds</span>
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider round"></span>
-            </label>
+        <div className="settings-left">
+          <div className="switches">
+            <div
+              className="switch-container onMouse"
+              onMouseEnter={() => setSpan("Sounds")}
+            >
+              <span className="switch-text">Sounds</span>
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider round"></span>
+              </label>
+            </div>
+
+            <div
+              className="switch-container onMouse"
+              onMouseEnter={() => setSpan("C1")}
+            >
+              <span className="switch-text">C1</span>
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider round"></span>
+              </label>
+            </div>
           </div>
-          <div className="switch-container">
-            <span className="switch-text">B2</span>
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider round"></span>
-            </label>
-            <span className="switch-text">C1</span>
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider round"></span>
-            </label>
+          <div
+            className="dailyGoal onMouse"
+            onMouseEnter={() => setSpan("DailyGoal")}
+          >
+            <span className="switch-text">daily progress</span>
+            <input
+              type="number"
+              value={newDailyGoal}
+              onChange={handleDailyGoalChange}
+            />
+          </div>
+          <div className="button-container-sett">
+            <button
+              style={{ "--buttonColor": "var(--tertiary)" }}
+              className="button"
+              onClick={saveSettings}
+            >
+              Save
+            </button>
           </div>
         </div>
-        <div className="inputs">
-          <input
-            type="number"
-            value={newDailyGoal}
-            onChange={handleDailyGoalChange}
-          />
+
+        <div className="settings-right">
+          <div className="explanation">
+            <span
+              className={`${activeSpan === "Sounds" ? "" : "hide-span-sett"}`}
+            >
+              Turn off sound effects.
+            </span>
+            <span className={`${activeSpan === "C1" ? "" : "hide-span-sett"}`}>
+              Adds a C1 word pool (another 2,000 words) and increases the total
+              progression.
+            </span>
+            <span
+              className={`${
+                activeSpan === "DailyGoal" ? "" : "hide-span-sett"
+              }`}
+            >
+              Change daily progress.
+            </span>
+          </div>
         </div>
-        <button className="button" onClick={saveSettings}>
-          Save
-        </button>
       </div>
       {popupMessage && (
         <Popup
