7157d8e7eb8d94094f4853f42d052b01b8de1f65
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-07-10 18:16:21 +0200

    sidebar 0.1

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { LuMenuSquare } from "react-icons/lu";
-import { FaGithub, FaBug } from "react-icons/fa";
 import { IoMdHome, IoMdSettings } from "react-icons/io";
 import { MdAccountBox, MdLogin, MdAdminPanelSettings } from "react-icons/md";
-import { FormattedMessage, useIntl } from "react-intl";
+import { FaBug, FaGithub } from "react-icons/fa";
+import { useIntl } from "react-intl";
 import api from "../../utils/api";
 import Popup from "../popup/popup";
 import ReportPopup from "../report/report-popup";
@@ -78,81 +77,19 @@ export default function Sidebar() {
   return (
     <>
       <div className="container-sidebar">
-        <div className="small-sidebar">
-          <LuMenuSquare />
-        </div>
-        <div className="big-sidebar">
-          <div className="title-sidebar">
-            <span>Memolingo</span>
-          </div>
-          <div className="links-sidebar">
-            <div className="links-container">
-              <div className="one" onClick={handleDivClick}>
-                <Link to="/home">
-                  <IoMdHome />
-                </Link>
-              </div>
-              <div className="two" onClick={handleDivClick}>
-                <Link to="/settings">
-                  <IoMdSettings />
-                </Link>
-              </div>
-              <div className="three" onClick={handleDivClick}>
-                <Link to="/login">
-                  <MdAccountBox />
-                </Link>
-              </div>
-              {isAdmin && isLoggedIn && (
-                <div className="four" onClick={handleDivClick}>
-                  <Link to="/admin">
-                    <MdAdminPanelSettings />
-                  </Link>
-                </div>
-              )}
-              <div className="five" onClick={handleDivClick}></div>
-              <div className="six" onClick={handleDivClick}></div>
-              <div className="seven" onClick={handleDivClick}></div>
-              <div className="eight" onClick={handleDivClick}></div>
-              <div className="nine" onClick={handleDivClick}></div>
-            </div>
-
-            <div>
-            {isLoggedIn && (
-                <div className="bug" onClick={() => setFormVisible(true)}>
-                  <FaBug />
-                </div>
-              )}
-            </div>
-
-          </div>
-          <div className="btn-container">
-            {isLoggedIn && (
-              <div onClick={logout} className="logout-button">
-                <span>Hi! {user.username} </span>
-                <MdLogin className="logout-icon" />
-                <div className="logout-text-container">
-                  <span className="logout-text">Log</span>
-                  <span className="logout-text">out</span>
-                </div>
-              </div>
-            )}
-          </div>
-          <a
-            href="https://github.com/Shafosphere"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="github-sidebar">
-              <div className="bar">
-                <div className="border"></div>
-              </div>
-              <FaGithub />
-              <div className="bar">
-                <div className="border"></div>
-              </div>
-            </div>
-          </a>
-        </div>
+        <SmallSidebar
+          isAdmin={isAdmin}
+          isLoggedIn={isLoggedIn}
+          handleDivClick={handleDivClick}
+        />
+        <BigSidebar
+          isLoggedIn={isLoggedIn}
+          setFormVisible={setFormVisible}
+          logout={logout}
+          user={user}
+          isAdmin={isAdmin}
+          handleDivClick={handleDivClick}
+        />
       </div>
       {popupMessage && (
         <Popup
@@ -166,5 +103,121 @@ export default function Sidebar() {
         onFormVisibleChange={handleFormVisibleChange}
       />
     </>
+  );
+}
+
+function SmallSidebar({ isAdmin, isLoggedIn, handleDivClick }) {
+  return (
+    <div className="small-sidebar">
+      {/* <div className="title-sidebar">
+        <span>Memolingo</span>
+      </div> */}
+      <PanelIcon
+        isAdmin={isAdmin}
+        isLoggedIn={isLoggedIn}
+        handleDivClick={handleDivClick}
+      />
+    </div>
+  );
+}
+
+function BigSidebar({
+  isLoggedIn,
+  setFormVisible,
+  logout,
+  user,
+  isAdmin,
+  handleDivClick,
+}) {
+  return (
+    <div className="big-sidebar">
+      {/* <div className="title-sidebar">
+        <span>Memolingo</span>
+      </div> */}
+      <PanelIcon
+        isAdmin={isAdmin}
+        isLoggedIn={isLoggedIn}
+        handleDivClick={handleDivClick}
+        setFormVisible={setFormVisible}
+        logout={logout}
+        user={user}
+      />
+    </div>
+  );
+}
+
+function PanelIcon({
+  isAdmin,
+  isLoggedIn,
+  handleDivClick,
+  setFormVisible,
+  logout,
+  user,
+}) {
+  return (
+    <div className="links-container">
+      <div className="one" onClick={handleDivClick}>
+        <Link to="/home">
+          <IoMdHome />
+        </Link>
+      </div>
+      <div className="two" onClick={handleDivClick}>
+        <Link to="/settings">
+          <IoMdSettings />
+        </Link>
+      </div>
+      <div className="three" onClick={handleDivClick}>
+        <Link to="/login">
+          <MdAccountBox />
+        </Link>
+      </div>
+      {isAdmin && isLoggedIn && (
+        <div className="four" onClick={handleDivClick}>
+          <Link to="/admin">
+            <MdAdminPanelSettings />
+          </Link>
+        </div>
+      )}
+      <div className="five"></div>
+      <div className="six">
+        <div>
+          {isLoggedIn && (
+            <div className="bug" onClick={() => setFormVisible(true)}>
+              <FaBug />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="seven">
+        <div className="btn-container">
+          {isLoggedIn && (
+            <div onClick={logout} className="logout-button">
+              <MdLogin className="logout-icon" />
+              <div className="logout-text-container">
+                <span className="logout-text">Log</span>
+                <span className="logout-text">out</span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="eight">
+        <a
+          href="https://github.com/Shafosphere"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="github-sidebar">
+            <div className="bar">
+              <div className="border"></div>
+            </div>
+            <FaGithub />
+            <div className="bar">
+              <div className="border"></div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
   );
 }
