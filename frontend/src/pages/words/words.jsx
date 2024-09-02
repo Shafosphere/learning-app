ae66bfbfcbfa4caa6c465ad53163d674e058bc92
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-09-02 19:53:50 +0200

    carousel 0.4

--- frontend/src/pages/words/words.jsx
+++ frontend/src/pages/words/words.jsx
@@ -9,6 +9,7 @@ export default function Words() {
   const [nextWordIndex, setnextWordIndex] = useState(0);
   const middleRef = useRef(null);
   const [loading, setLoading] = useState(true);
+  // const [tempLastFour, setTempLastFour] = useState(null);
 
   const [carousel, setCarousel] = useState({
     1: "top-bot",
@@ -28,14 +29,22 @@ export default function Words() {
     newCarousel[1] = carousel[5];
 
     setCarousel(newCarousel);
+
+    const middleDiv = document.querySelector(".middle");
+    if (middleDiv) {
+      console.log("Middle div found:", middleDiv.id); // Logs the ID of the current middle div
+      middleRef.current = middleDiv; // Store the current middle div in the ref
+    }
+
   };
 
   useEffect(() => {
+    console.log('startGame')
     async function startGame() {
       const gameData = await getData();
       if (gameData && gameData.data && gameData.data.length > 0) {
         setData(gameData.data);
-        setnextWordIndex(3);
+        setnextWordIndex(2);
         setLoading(false);
       } else {
         console.log("Brak danych");
@@ -45,44 +54,38 @@ export default function Words() {
     startGame();
   }, []);
 
-  useEffect(() => {
-    if (!loading && data.length > 0) {
-      // Sprawdź czy loading jest false i dane są dostępne
-      function updateDivs() {
-        const div3 = document.getElementById("3");
-        const div4 = document.getElementById("4");
-        const div5 = document.getElementById("5");
-  
-        if (div3) {
-          div3.textContent = data[0].wordEng.word;
-          div3.dataset.id = data[0].id; // Ustaw atrybut data-id
-          div3.dataset.translate = data[0].wordPl.word; // Ustaw atrybut data-translate
-        }
-        if (div4) {
-          div4.textContent = data[1].wordEng.word;
-          div4.dataset.id = data[1].id; // Ustaw atrybut data-id
-          div4.dataset.translate = data[1].wordPl.word; // Ustaw atrybut data-translate
-        }
-        if (div5) {
-          div5.textContent = data[2].wordEng.word;
-          div5.dataset.id = data[2].id; // Ustaw atrybut data-id
-          div5.dataset.translate = data[2].wordPl.word; // Ustaw atrybut data-translate
-        }
-      }
-      updateDivs(); // Wywołaj updateDivs tylko raz, kiedy loading zmieni się na false
-    }
-  }, [loading, data]); // Obserwuj loading i data, aby updateDivs wykonało się raz po załadowaniu danych
+  // useEffect(() => {
+  //   if (!loading && data.length > 0) {
 
-  useEffect(() => {
-    // Whenever carousel changes, update the reference to the middle div
-    const middleDiv = document.querySelector(".middle");
-    if (middleDiv) {
-      console.log("Middle div found:", middleDiv.id); // Logs the ID of the current middle div
-      middleRef.current = middleDiv; // Store the current middle div in the ref
-    }
-  }, [carousel]);
+  //     function updateDivs() {
+  //       const div3 = document.getElementById("3");
+  //       const div4 = document.getElementById("4");
+  //       const div5 = document.getElementById("5");
+
+  //       if (div3) {
+  //         div3.textContent = data[0].wordEng.word;
+  //         div3.dataset.id = data[0].id;
+  //         div3.dataset.translate = data[0].wordPl.word;
+  //       }
+  //       if (div4) {
+  //         div4.textContent = data[1].wordEng.word;
+  //         div4.dataset.id = data[1].id;
+  //         div4.dataset.translate = data[1].wordPl.word;
+  //       }
+  //       if (div5) {
+  //         div5.textContent = data[2].wordEng.word;
+  //         div5.dataset.id = data[2].id;
+  //         div5.dataset.translate = data[2].wordPl.word;
+  //       }
+  //       setLoading(false);
+  //     }
+
+  //     updateDivs();
+  //   }
+  // }, [loading, data]);
 
   async function getData() {
+    console.log('getdata')
     let maxWordId;
     let minWordId;
 
@@ -115,32 +118,77 @@ export default function Words() {
     setWord(event.target.value);
   }
 
+  //enter
   function handleKeyDown(event) {
+
+    console.log('enter')
     if (event.key === "Enter") {
+      //jesli istnieją dane
       if (data && data.length > 0) {
+        //pobierz srodkowy div
         const middleDiv = document.querySelector(".middle");
+        //jesli słowo wpisane równe jest poprawnej odpowiedzi
         if (currentWord === middleDiv.dataset.translate) {
           addNumberToGood(middleDiv.dataset.id);
         } else {
+          //w przeciwnym wypadku
           addNumberToWrong(middleDiv.dataset.id);
         }
+
+        //znajdz dolnego diva
         const bottomBotDiv = document.querySelector(".bottom-bot");
         if (bottomBotDiv) {
+          //jezeli znalazłes to zaktualizuj jego dane
           bottomBotDiv.textContent = data[nextWordIndex].wordEng.word;
-          bottomBotDiv.dataset.id = data[nextWordIndex].id; // Użyj dataset dla spójności
-          bottomBotDiv.dataset.translate = data[nextWordIndex].wordPl.word; // Użyj dataset dla spójności
+          bottomBotDiv.dataset.id = data[nextWordIndex].id;
+          bottomBotDiv.dataset.translate = data[nextWordIndex].wordPl.word;
         }
-        setnextWordIndex((prevIndex) =>
-          Math.min(prevIndex + 1, data.length - 1)
-        );
-  
-        handleClick();
+
+
+        setnextWordIndex((prevIndex) => {
+          // pobierz aktualna wartosc nextWordIndex
+          // jezeli nextWordIndex jest ostatnie
+          //pobierz nowe dane
+          if (prevIndex >= data.length - 1) {
+            console.log('reset');
+            
+            // async function reset() {
+            //   const gameData = await getData();
+            //   if (gameData && gameData.data && gameData.data.length > 0) {
+            //     setData(gameData.data);
+            //   } 
+            // }
+
+            //zapisuje nowe dane w data
+            // reset();
+            //ustawia index na 0
+            return 0;
+          } else {
+            //ustawia index na kolejny
+            if(prevIndex + 1 >= data.length - 1 ){
+              async function reset() {
+                const gameData = await getData();
+                if (gameData && gameData.data && gameData.data.length > 0) {
+                  setData(gameData.data);
+                } 
+              }
+              //zapisuje nowe dane w data
+              reset();
+            }
+            return prevIndex + 1;
+          }
+
+        });
+        
+
+        setWord("");
       } else {
         console.log("Źle");
       }
+      handleClick();
     }
   }
-  
+
   function random() {
     const middleDiv = document.querySelector(".middle");
     const output = middleDiv.dataset.translate;
@@ -168,11 +216,14 @@ export default function Words() {
           </div>
         </div>
         <div className="bot-words">
-          {data && data[nextWordIndex] && (
-            <span>{random()}</span>
-          )}
+          {data && data[nextWordIndex] && <span>{random()}</span>}
         </div>
       </div>
     </div>
   );
 }
+
+// notatka na jutro:
+// po usunieciu use efect z loading i data nie aktuyalizuje juz automatycznie divów
+// ale: nie wyswietla 2 pierwszych (tołatwe, wystarczy dac na 0)
+// nie pokazuje ostatnich wartosci
