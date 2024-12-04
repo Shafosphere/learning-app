090ec0752c979b9a6196f14b7946fb83156ab666
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-12-04 17:11:43 +0100

    update frontend ranking

--- frontend/src/components/ranking/rankingcontent.jsx
+++ frontend/src/components/ranking/rankingcontent.jsx
@@ -3,6 +3,10 @@ import styles from "./ranking.module.css";
 import api from "../../utils/api";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Loading from "../../components/loading/loading";
+import gold from "../../data/medal/gold.png";
+import silver from "../../data/medal/silver.png";
+import bronze from "../../data/medal/bronze.png";
+import avatar from "../../data/avatars/man.png";
 
 export default function RankingContent() {
   const [data, setData] = useState(null);
@@ -36,33 +40,71 @@ export default function RankingContent() {
           endMessage={<div></div>}
           scrollableTarget="scrollableDiv"
         >
-          <table className={styles.table}>
-            <thead className={styles.thead}>
-              <tr>
+          <table>
+            <thead className={styles.tablehead}>
+              <tr className={styles.rowhead}>
                 <th>position</th>
                 <th>nickname</th>
                 <th>Points</th>
               </tr>
             </thead>
             <tbody>
-              {data.map((item, index) =>
-                index === 0 ? (
+              {data.map((item, index) => {
+                let medal = null;
+
+                if (index === 0) {
+                  medal = (
+                    <img
+                      alt="gold_medal"
+                      className={`${styles.gold} ${styles.medal}`}
+                      src={gold}
+                    />
+                  );
+                } else if (index === 1) {
+                  medal = (
+                    <img
+                      alt="silver_medal"
+                      className={`${styles.silver} ${styles.medal}`}
+                      src={silver}
+                    />
+                  );
+                } else if (index === 2) {
+                  medal = (
+                    <img
+                      alt="bronze_medal"
+                      className={`${styles.bronze} ${styles.medal}`}
+                      src={bronze}
+                    />
+                  );
+                }
+
+                return (
                   <tr
                     key={item.id}
-                    className={`${styles.firstrow} ${styles.row}`}
+                    className={`${styles.row} ${
+                      index === 0 ? styles.firstrow : ""
+                    }`}
                   >
-                    <td>{index + 1}</td>
-                    <td>{item.username}</td>
+                    <td>
+                      {index + 1}.{medal && medal}
+                    </td>
+                    <td>
+                      <div className={styles.userInfo}>
+                        <div>
+                          <img
+                            alt="avatar"
+                            className={styles.avatar}
+                            src={avatar}
+                          />
+                        </div>
+                        <div>{item.username}</div>
+                      </div>
+                    </td>
+
                     <td>{item.weekly_points}</td>
                   </tr>
-                ) : (
-                  <tr className={styles.row} key={item.id}>
-                    <td>{index + 1}</td>
-                    <td>{item.username}</td>
-                    <td>{item.weekly_points}</td>
-                  </tr>
-                )
-              )}
+                );
+              })}
             </tbody>
           </table>
         </InfiniteScroll>
