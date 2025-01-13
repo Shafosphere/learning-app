e360adc3c7164eddc595692f2442175c89abe909
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2025-01-13 19:31:43 +0100

    settings mobile version

--- frontend/src/pages/settings/settings.jsx
+++ frontend/src/pages/settings/settings.jsx
@@ -23,14 +23,12 @@ export default function Settings() {
     diacritical,
     setDiacritical,
     spellChecking,
-    setSpellChecking
+    setSpellChecking,
   } = useContext(SettingsContext);
 
   //popup
   const { setPopup } = useContext(PopupContext);
-
   const [bookMark, setBookMark] = useState(1);
-
   const [newDailyGoal, setNewDailyGoal] = useState(dailyGoal);
 
   // confirm
@@ -69,69 +67,14 @@ export default function Settings() {
     setConfirmCallback(null);
   };
 
-  // function clearBoxes() {
-  //   return new Promise((resolve, reject) => {
-  //     let db;
-  //     const request = indexedDB.open("MyTestDatabase", 2);
-
-  //     request.onupgradeneeded = (event) => {
-  //       console.log("onupgradeneeded event fired");
-  //       db = event.target.result;
-  //       console.log("Upgrading database to version", db.version);
-  //       if (!db.objectStoreNames.contains("boxes")) {
-  //         db.createObjectStore("boxes", { keyPath: "id" });
-  //         console.log('Object store "boxes" created.');
-  //       } else {
-  //         console.log('Object store "boxes" already exists.');
-  //       }
-  //     };
-
-  //     request.onsuccess = (event) => {
-  //       console.log("onsuccess event fired");
-  //       db = event.target.result;
-  //       console.log("Database opened successfully");
-  //       if (db.objectStoreNames.contains("boxes")) {
-  //         const transaction = db.transaction(["boxes"], "readwrite");
-  //         const store = transaction.objectStore("boxes");
-
-  //         const clearRequest = store.clear();
-
-  //         clearRequest.onsuccess = () => {
-  //           console.log('Object store "boxes" has been cleared.');
-  //           resolve(); // Zakończ Promise po pomyślnym wyczyszczeniu
-  //         };
-
-  //         clearRequest.onerror = () => {
-  //           console.error('Error clearing the object store "boxes".');
-  //           reject('Error clearing the object store "boxes".'); // Zakończ Promise z błędem
-  //         };
-  //       } else {
-  //         console.error("Object store 'boxes' does not exist.");
-  //         reject("Object store 'boxes' does not exist."); // Zakończ Promise z błędem
-  //       }
-  //     };
-
-  //     request.onerror = (event) => {
-  //       console.error("IndexedDB error:", event.target.error);
-  //       reject("IndexedDB error: " + event.target.error); // Zakończ Promise z błędem
-  //     };
-  //   });
-  // }
-
-  // function clearProgress() {
-  //   let wordIds = [];
-  //   localStorage.setItem("wordIds", JSON.stringify(wordIds));
-  //   localStorage.setItem("totalPercent", JSON.stringify(0));
-  //   localStorage.setItem("lastID", JSON.stringify(null));
-  //   localStorage.setItem("dailyGoal", JSON.stringify("20"));
-  // }
-
   async function clearEverything() {
     try {
       localStorage.clear();
       const deleteIndexedDB = () => {
-        const dbs = indexedDB.databases ? indexedDB.databases() : Promise.resolve([]);
-        
+        const dbs = indexedDB.databases
+          ? indexedDB.databases()
+          : Promise.resolve([]);
+
         dbs.then((databases) => {
           databases.forEach((db) => {
             indexedDB.deleteDatabase(db.name);
@@ -139,9 +82,8 @@ export default function Settings() {
           });
         });
       };
-  
-      deleteIndexedDB();
 
+      deleteIndexedDB();
 
       window.location.reload();
     } catch (error) {
@@ -150,99 +92,84 @@ export default function Settings() {
   }
 
   return (
-    <div className="container-settings">
-      <div>
+    <div className="container-settings1">
+      {/* <div>
+        <div className="bookmarks-sett">
+          <div onClick={() => setBookMark(1)}>1</div>
+          <div onClick={() => setBookMark(2)}>2</div>
+        </div> */}
+
+      <div className="window-settings">
+
         <div className="bookmarks-sett">
           <div onClick={() => setBookMark(1)}>1</div>
           <div onClick={() => setBookMark(2)}>2</div>
         </div>
 
-        <div className="window-settings">
-          <div className="settings-left">
-            {bookMark === 1 ? (
-              <FirstBookMark
-                activeSpan={activeSpan}
-                setSpan={setSpan}
-                toggleSound={toggleSound}
-                isSoundEnabled={isSoundEnabled}
-                toggleTheme={toggleTheme}
-                themeMode={themeMode}
-                toggleLevel={toggleLevel}
-                level={level}
-                language={language}
-                setLanguage={setLanguage}
-                showConfirm={showConfirm}
-                clearEverything={clearEverything}
-                handleDailyGoalChange={handleDailyGoalChange}
-                newDailyGoal={newDailyGoal}
-                saveSettings={saveSettings}
-              />
-            ) : (
-              <SecondBookMark
-                diacritical={diacritical}
-                setDiacritical={setDiacritical}
-                setSpan={setSpan}
-                spellChecking={spellChecking}
-                setSpellChecking={setSpellChecking}
-              />
-            )}
-          </div>
-
-          {/* descriptions */}
-          <div className="settings-right">
-            <div className="explanation">
-              {(() => {
-                const contentMap = {
-                  Sounds: <FormattedMessage id="turnOffSoundEffects" />,
-                  C1: <FormattedMessage id="addsC1WordPool" />,
-                  DailyGoal: <FormattedMessage id="changeDailyProgress" />,
-                  darkmode: <FormattedMessage id="mode" />,
-                  resetsbuttons: (
-                    <span>
-                      <p>
-                        <FormattedMessage id="resetButtons" />
-                      </p>
-                      <ul className="reset-list">
-                        <li>
-                          - <FormattedMessage id="resetBoxesDescription" />
-                        </li>
-                        <li>
-                          - <FormattedMessage id="resetProgressDescription" />
-                        </li>
-                        <li>
-                          - <FormattedMessage id="resetEverythingDescription" />
-                        </li>
-                      </ul>
-                    </span>
-                  ),
-                  language: <FormattedMessage id="changeLanguage" />,
-                  diacritical: (
-                    
+        <div className="settings-left">
+          {bookMark === 1 ? (
+            <FirstBookMark
+              activeSpan={activeSpan}
+              setSpan={setSpan}
+              toggleSound={toggleSound}
+              isSoundEnabled={isSoundEnabled}
+              toggleTheme={toggleTheme}
+              themeMode={themeMode}
+              toggleLevel={toggleLevel}
+              level={level}
+              language={language}
+              setLanguage={setLanguage}
+              showConfirm={showConfirm}
+              clearEverything={clearEverything}
+              handleDailyGoalChange={handleDailyGoalChange}
+              newDailyGoal={newDailyGoal}
+              saveSettings={saveSettings}
+            />
+          ) : (
+            <SecondBookMark
+              diacritical={diacritical}
+              setDiacritical={setDiacritical}
+              setSpan={setSpan}
+              spellChecking={spellChecking}
+              setSpellChecking={setSpellChecking}
+            />
+          )}
+        </div>
+        {/* descriptions */}
+        <div className="settings-right">
+          <div className="explanation">
+            {(() => {
+              const contentMap = {
+                Sounds: <FormattedMessage id="turnOffSoundEffects" />,
+                C1: <FormattedMessage id="addsC1WordPool" />,
+                DailyGoal: <FormattedMessage id="changeDailyProgress" />,
+                darkmode: <FormattedMessage id="mode" />,
+                resetsbuttons: (
+                  <span>
                     <p>
-                      <h2>działa na voca test i fiszki!</h2>
-                      <br /> 
-                      przy sprawdzaniu wyników traktuje:
-                      <br /> "ą" jako "a"
-                      <br /> "ę" jako "e"
-                      <br /> "ź" jako "z"
-                      <br /> itd
+                      <FormattedMessage id="resetButtons" />
                     </p>
-                  ),
-                  spellChecking: (
-                    <p>
-                      <h2>działa na voca test i fiszki!</h2>
-                      <br /> 
-                      bedziesz mógł pomylić sie o 1 literke
-                    </p>
-                  )
-                };
+                  </span>
+                ),
+                language: <FormattedMessage id="changeLanguage" />,
+                diacritical: (
+                  <p>
+                    ignoruje znaki diakrytyczne w polskim
+                  </p>
+                ),
+                spellChecking: (
+                  <p>
+                    bedziesz mógł pomylić sie o 1 literke
+                  </p>
+                ),
+              };
 
-                return contentMap[activeSpan] || null;
-              })()}
-            </div>
+              return contentMap[activeSpan] || null;
+            })()}
           </div>
         </div>
       </div>
+      {/* </div> */}
       {confirmMessage && (
         <ConfirmWindow message={confirmMessage} onClose={handleConfirmClose} />
       )}
