c45183e4473777a8f8ff05c89c6118b03524ddbe
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2025-01-24 18:54:56 +0100

    arrow navigation boxes

--- frontend/src/components/maingame/box/box.jsx
+++ frontend/src/components/maingame/box/box.jsx
@@ -6,21 +6,41 @@ import box from "../../../data/resized_box.png";
 import "./box.css";
 import { FormattedMessage } from "react-intl";
 import MyButton from "../../button/button";
+import React, { useEffect } from "react";
 
 export default function Boxes({ boxes, activeBox, handleSetBox, addWords }) {
   const boxNames = ["boxOne", "boxTwo", "boxThree", "boxFour", "boxFive"];
 
-  function boxImage(words){
+  function boxImage(words) {
     if (words === 0) {
       return box;
-    } else if (words >0 && words < 30){
+    } else if (words > 0 && words < 30) {
       return boxSome;
-    } else if(words >= 30 && words < 60){
+    } else if (words >= 30 && words < 60) {
       return boxHalf;
     } else {
       return boxFull;
     }
   }
+
+  useEffect(() => {
+    function handleKeyDown(event) {
+      const boxes = ["boxOne", "boxTwo", "boxThree", "boxFour", "boxFive"];
+      const index = boxes.indexOf(activeBox);
+
+      if (event.key === "ArrowRight") {
+        handleSetBox(boxes[(index + 1) % boxes.length]);
+      } else if (event.key === "ArrowLeft") {
+        handleSetBox(boxes[(index + boxes.length - 1) % boxes.length]);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [activeBox, handleSetBox]);
 
   return (
     <div className="container-boxes">
@@ -40,11 +60,7 @@ export default function Boxes({ boxes, activeBox, handleSetBox, addWords }) {
           </div>
         ))}
         <div className="smallscreenbutton">
-          <MyButton
-            message="add"
-            color="yellow"
-            onClick={addWords}
-          />
+          <MyButton message="add" color="yellow" onClick={addWords} />
         </div>
       </div>
 
