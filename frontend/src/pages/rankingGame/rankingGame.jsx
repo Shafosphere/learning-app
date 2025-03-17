021405ccfc7f543261542d705888ce7455038a5f
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2025-03-17 19:25:27 +0100

    ranked game 1.0

--- frontend/src/pages/rankingGame/rankingGame.jsx
+++ frontend/src/pages/rankingGame/rankingGame.jsx
@@ -1,36 +1,55 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import Loading from "../../components/loading/loading";
 import MyButton from "../../components/button/button";
 import "./rankinggmae.css";
 import { FaTrophy } from "react-icons/fa";
+import { SettingsContext } from "../settings/properties";
+import { useNavigate } from "react-router-dom";
 const RankingGameContent = React.lazy(() =>
   import("../../components/rankinggame/rankinggame")
 );
 
 export default function RankingGame() {
   const [gameStarted, setGameStarted] = useState(false);
+  const {isLoggedIn} = useContext(SettingsContext)
+  const navigate = useNavigate();
+
+  function start(){
+    if(!isLoggedIn){
+      console.log("sprawdzam " + isLoggedIn);
+      navigate("/login");
+    } else {
+      setGameStarted(true);
+    }
+  }
 
   return (
     <div className="rankinggame-container">
-      {!gameStarted && (
-        <div>
-          <FaTrophy  className="icon-rankinggame"/>
-          {/* <div>
-            <span>Ranking Game</span>
-            <MyButton
-              message="start game"
-              color="green"
-              onClick={() => setGameStarted(true)}
-            />
-          </div> */}
-        </div>
-      )}
+      <div className="rankinggame-select-window">
+        {!gameStarted && (
+          <div className="rankinggame-trophy">
+            <FaTrophy className="icon-rankinggame" />
+            <div className="rankinggame-trophy-text">
+              <span>gra </span>
+              <span>rywalizacyjna</span>
+            </div>
+            <div className="rankinggame-trophy-button">
+              <span>(tylko dla zalogowanych)</span>
+              <MyButton
+                message="start gry"
+                color="green"
+                onClick={() => start()}
+              />
+            </div>
+          </div>
+        )}
 
-      {gameStarted && (
-        <Suspense fallback={<Loading />}>
-          <RankingGameContent />
-        </Suspense>
-      )}
+        {gameStarted && (
+          <Suspense fallback={<Loading />}>
+            <RankingGameContent />
+          </Suspense>
+        )}
+      </div>
     </div>
   );
 }
